583cf66f77c8fa3f1dfb22f032e1b952a7b63bc2
sofiarapetti <srapetti@frba.utn.edu.ar>
2025-05-06 20:06:34 -0300

    punto 1 sin test

--- src/Library.hs
+++ src/Library.hs
@@ -2,7 +2,7 @@ module Library where
 import PdePreludat
 
 data Ingrediente =
-    Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras
+    Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras | BaconDeTofu
     deriving (Eq, Show)
 
 precioIngrediente Carne = 20
@@ -12,21 +12,45 @@ precioIngrediente Cheddar = 10
 precioIngrediente Pollo =  10
 precioIngrediente Curry = 5
 precioIngrediente QuesoDeAlmendras = 15
+precioIngrediente BaconDeTofu = 12
 
 data Hamburguesa = Hamburguesa {
     precioBase :: Number,
     ingredientes :: [Ingrediente]
 } deriving (Eq, Show)
 
+cuartoDeLibra :: Hamburguesa 
+cuartoDeLibra = Hamburguesa 20 [Pan, Carne, Cheddar, Pan]
+
 agrandar :: Hamburguesa -> Hamburguesa
-agrandar hamburguesa | esDeCarne Hamburguesa = agregarIngrediente Carne 
-                     | esDePollo Hamburguesa = agregarIngrediente Pollo
+agrandar hamburguesa | ingredienteBase hamburguesa == Carne = agregarIngrediente Carne hamburguesa
+                     | ingredienteBase hamburguesa == Pollo = agregarIngrediente Pollo hamburguesa
 
-esDeCarne :: Hamburguesa -> Bool
-esDeCarne (Hamburguesa _ ingredientes) = elem Carne ingredientes
-
-esDePollo :: Hamburguesa -> Bool 
-esDePollo (Hamburguesa _ ingredientes) = elem Pollo ingredientes
+ingredienteBase :: Hamburguesa -> Ingrediente
+ingredienteBase (Hamburguesa _ ingredientes) | elem Pollo ingredientes = Pollo
+                                             | otherwise = Carne
 
 agregarIngrediente :: Ingrediente -> Hamburguesa -> Hamburguesa
-agr
+agregarIngrediente ingrediente (Hamburguesa precioBase ingredientes) 
+    = Hamburguesa (cambiarPrecio precioBase ingrediente) (ingrediente:ingredientes)
+
+cambiarPrecio :: Number -> Ingrediente -> Number
+cambiarPrecio precioBase ingrediente =  precioBase + precioIngrediente ingrediente
+
+descuento :: Number -> Hamburguesa -> Hamburguesa
+descuento porcentaje hamburguesa 
+    = modificarPrecioBase (calcularDescuento porcentaje(precioBase hamburguesa)) hamburguesa
+
+calcularDescuento :: Number -> Number -> Number
+calcularDescuento descuento precioBase = (1- descuento/100) * precioBase 
+
+modificarPrecioBase :: Number -> Hamburguesa -> Hamburguesa
+modificarPrecioBase nuevoPrecio (Hamburguesa precioViejo ingredientes) 
+    = Hamburguesa nuevoPrecio ingredientes
+
+pdepBurger :: Hamburguesa 
+pdepBurger = (agregarIngrediente Cheddar .agregarIngrediente Panceta .agregarIngrediente Panceta . agrandar . agrandar)cuartoDeLibra
+
+
+
+  
